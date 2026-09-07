9cf25f1021bf61b60b6a84f6061677161eda8812
Yousef-massad <yousef.d.massad@gmail.com>
2026-09-07 15:28:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+console.log("                     ")
+
+for (var i = 100; i >= 0; i--) {
+    if (i % 3 === 0) {
+        console.log(i);
+    }
+}
+
+console.log("                     ")
+
+for (var i =4; i>=-3.5; i-=1.5){
+    console.log(i);
+}
+
+console.log("                     ")
+
+
+var sum = 0
+for(var i = 1; i<=100; i++){
+     sum = sum + i;
+}
+console.log(sum);
+
+console.log("                     ")
+
+var product = 1;
+for(var i=1; i<=12; i++){
+    product *=i;
+}
+console.log(product);
